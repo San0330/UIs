3eed0f4c3099af38a63c2aeffb66c8a60a7cffa3
San0330 <sanneupane1024@gmail.com>
2022-11-27 09:56:31 +0545

    upated chat sidenav

--- chats/src/App.js
+++ chats/src/App.js
@@ -1,8 +1,20 @@
+import Sidenav from "./components/sidenav";
 
 function App() {
     return (
-        <div class='text-3xl text-red-800'>
-            Hello world
+        <div className='flex flex-row'>
+            <div className='flex-1 bg-gray-400'>
+                <Sidenav />
+            </div>
+            <div className='flex-3 bg-red-400'>
+                Chat Users List
+            </div>
+            <div className='flex-9 bg-blue-400'>
+                Chats
+            </div>
+            <div className='flex-3 bg-green-400'>
+                chat user settings
+            </div>
         </div>
     );
 }
